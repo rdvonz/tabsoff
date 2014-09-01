6e0f269b473ff028d07893ab644819edd619e035
Robert Von Zimmerman <rdvonz@gmail.com>
2014-09-01 15:47:43 -0700

    name change!

--- msync/library/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from library.models import Track
-
-class TrackAdmin(admin.ModelAdmin):
-    list_display = ('number', 'title', 'artist', 'album')
-
-admin.site.register(Track, TrackAdmin)
