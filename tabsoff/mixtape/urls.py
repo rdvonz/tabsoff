af855e6b38fd9cc775994198e454f604728890f9
Robert Von Zimmerman <rdvonz@gmail.com>
2014-09-04 13:59:14 -0700

    custom authentication, allows redirecting to homepage

--- tabsoff/mixtape/urls.py
+++ tabsoff/mixtape/urls.py
@@ -3,11 +3,12 @@ from django.contrib.auth.decorators import login_required
 from mixtape import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.MixTapeList.as_view()),
+                       url(r'^$', views.MixTapeList.as_view(), name='index'),
                        url(r'^(?P<pk>\d+)/$', views.MixTapeDetail.as_view(), name='mixtape'),
-                       url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'mixtape/login.html'}, name='login'),
-                       url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
+                       url(r'^login/$', views.anywhere_login, name='login'),
+                       url(r'^logout/$', views.anywhere_logout, name='logout'),
                        url(r'^upload/$', views.upload, name='upload'),
+                       url(r'^login-required', views.not_logged_in, name='login_required'),
                        url(r'^mixes', login_required(views.UserMixTapeList.as_view()), name='user_mixes'),
                        url(r'^add-favorite/(?P<pk>\d+)/$', views.add_favorite, name='favorite'),
                        url(r'^favorites', login_required(views.FavoriteMixTapeList.as_view()), name='favorite_mixes'),
--- tabsoff/mixtape/views.py
+++ tabsoff/mixtape/views.py
@@ -4,11 +4,12 @@ from mutagen.mp3 import HeaderNotFoundError
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.core.files import File
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView
 from mixtape.models import MixTape, Track
 from mixtape.forms import MixTapeUploadForm
-from PIL import Image
 
 class MixTapeList(ListView):
     '''
@@ -33,13 +34,17 @@ class UserMixTapeList(ListView):
 
 class MixTapeDetail(DetailView):
     '''
+    'registration',
     the mixtape page
     '''
     template_name = 'mixtape/mixtape-detail.html'
     #we're only displaying a single mixtape here, and we're building the url based off the mixtape id (the primary key)
     model = MixTape
 
+def not_logged_in(request):
+    mixtape = MixTape.objects.all()
 
+    return render(request, 'mixtape/mixtape.html', {'mixtapes': mixtape, 'no_entry': True})
 
 def upload(request):
     '''
@@ -58,18 +63,26 @@ def upload(request):
         if upload_form.is_valid():
             #when we know we're dealing with a bound form, create a new model based on the form data
 
-            mix_zipfile = request.FILES['mixtape']
+            #The easiest thing to do is to create the new MixTape object, we need it to create each track
             new_mix = MixTape(title=request.POST['mix_name'], album_art=request.FILES['album_art'], created_by=request.user)
             new_mix.save()
+
+            #Next we check if we're really dealing with a zipfile, else we give the user an error message
+            mix_zipfile = request.FILES['mixtape']
             try:
                 mixtape_files = zipfile.ZipFile(mix_zipfile)
                 mixtape_files.extractall()
             except zipfile.BadZipfile:
+
                 return render(request, 'mixtape/upload.html', {'upload_form': upload_form, 'error_zip':True})
 
             for filename in mixtape_files.namelist():
                     current_file = os.path.join(os.getcwd(), filename)
-                    f = open(current_file)
+                    try:
+                        f = open(current_file)
+                    except IOError:
+                        return render(request, 'mixtape/upload.html', {'upload_form': upload_form, 'error_dir':True, 'song_error': filename})
+                        os.remove(current_file)
                     try:
                         mixtape_files.extractall()
                         music_tags = MP3(os.path.join(os.getcwd(), filename))
@@ -83,9 +96,12 @@ def upload(request):
                         track.save()
                         f.close()
                         os.remove(current_file)
+
+                    #A header not found error means we don't have an mp3 file
                     except HeaderNotFoundError:
-                    #any errors would mean they didn't upload a proper mp3
                         return render(request, 'mixtape/upload.html', {'upload_form': upload_form, 'error_mp3':True, 'song_error': filename})
+
+                    #this is indicative of a missing tag
                     except KeyError:
                         return render(request, 'mixtape/upload.html', {'upload_form': upload_form, 'error_tag':True, 'song_error': filename})
 
@@ -93,8 +109,26 @@ def upload(request):
 
     return render(request, 'mixtape/upload.html', {'upload_form': upload_form, 'error': False}, )
 
-
+@login_required()
 def add_favorite(request, pk):
-    mixtape = MixTape.objects.get(pk=pk) mixtape.favorited_by.add(request.user)
+    mixtape = MixTape.objects.get(pk=pk)
+    mixtape.favorited_by.add(request.user)
     return HttpResponseRedirect(reverse('mixtapes:mixtape', args=(pk,)))
 
+def anywhere_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            print request.path
+            return HttpResponseRedirect(reverse('mixtapes:index'))
+        else:
+            pass
+    else:
+        return HttpResponseRedirect(reverse('mixtapes:index'))
+
+def anywhere_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('mixtapes:index'))
