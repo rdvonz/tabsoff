08693e91e5ea01c341b124391a932b3723d29b0e
Robert Von Zimmerman <rdvonz@gmail.com>
2014-09-03 18:21:58 -0700

    added 'mixes' and 'add-favorite' view

--- tabsoff/mixtape/urls.py
+++ tabsoff/mixtape/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
                        url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'mixtape/login.html'}, name='login'),
                        url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
                        url(r'^upload/$', views.upload, name='upload'),
-                       url(r'^mixes', views.user_mixes, name='user_mixes')
+                       url(r'^mixes', views.user_mixes, name='user_mixes'),
+                       url(r'^add-favorite/(?P<pk>\d+)/$', views.add_favorite, name='favorite')
                        )
